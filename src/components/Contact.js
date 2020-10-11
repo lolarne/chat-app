69f9c001df6d544d4a6fa22305463941b0766cb4
lolarne <rne.lola@gmail.com>
2020-10-11 17:27:13 +0200

    added user profil

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Contact.css';
+
+const name = "Theresa Roberts";
+const avatar = "https://randomuser.me/api/portraits/women/32.jpg";
+const status = true;
+
+function Contact(){
+    return(
+        <div className="Contact">
+            <img src={avatar} className="avatar"></img>
+            <div>
+                <h4 className="name">{name}</h4>
+                <div className="status">
+                    <p><span className="status-online"></span>{status ? "Online" : "Offline"}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
