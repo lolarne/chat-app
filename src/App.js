c9dfa4ca8dd8c472288daa017957b0624a8c6186
lolarne <rne.lola@gmail.com>
2020-10-12 15:50:47 +0200

    changes on App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-      name="ana Owens"
-      avatar="https://randomuser.me/api/portraits/women/32.jpg"
-      status= {true}
-      />
-      <Contact
-      name="ana Owens"
-      avatar="https://randomuser.me/api/portraits/women/12.jpg"
-      status= {false}
-      />
-      <Contact
-      name="Daryl Rhodes"
-      avatar="https://randomuser.me/api/portraits/men/46.jpg"
-      status= {true}
-      />
+      <ContactList/>
     </div>
   );
 }
